ef776fde9d3a7ab4d47604d83df4e30780d34c69
Acer Iverson <acer.iverson@gmail.com>
2024-12-28 08:29:51 -0600

    Debug hub.verify_token for establishing webhook

--- backend/titles/get_webhook.go
+++ backend/titles/get_webhook.go
@@ -2,10 +2,12 @@ package titles
 
 import (
 	"errors"
+	"fmt"
 	"os"
 )
 
 func (h *TitlesCore) GetWebhook(verify_token string) error {
+	fmt.Println("verify_token:", verify_token)
 	if verify_token == os.Getenv("STRAVA_WEBHOOK_VERIFY_TOKEN") {
 		return nil
 	}
